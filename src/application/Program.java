3078aca4b037eec72bf711c088aedd609a7fdffc
Diego Santana <3c.santana@gmail.com>
2024-03-18 11:53:46 -0300

    Atualizando dados com o JDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,39 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DbException;
+
 public class Program {
 
 	public static void main(String[] args) {
-
+			Connection conn = null;
+			PreparedStatement preparedSt = null;
+			
+			try {
+				conn = DB.connectDb();
+				preparedSt = conn.prepareStatement(
+						"UPDATE seller "
+						+ "SET BaseSalary = BaseSalary + ? "
+						+ "WHERE "
+						+ "DepartmentId = ?"
+						);
+				preparedSt.setDouble(1, 500.50);
+				preparedSt.setInt(2, 2);
+				
+				int changedRows = preparedSt.executeUpdate();
+				
+				System.out.println("Done! Rowns affected: " + changedRows);
+				
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			} finally {
+				DB.closeStatement(preparedSt);
+				DB.closeConnection();
+			}
 	}
 
 }
